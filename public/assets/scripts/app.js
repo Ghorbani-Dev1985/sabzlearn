b5079ca3118291e63f9dfcec19ba4c51520d5014
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-30 22:07:56 +0330

    feat: develop course card component and add cards to main page

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,4 +1,4 @@
 import { Header } from "../../Components/Header/Header.js";
-
+import { CourseCard } from "../../Components/CourseCard/CourseCard.js";
 window.customElements.define('site-header' , Header);
-
+window.customElements.define('course-card' , CourseCard);
