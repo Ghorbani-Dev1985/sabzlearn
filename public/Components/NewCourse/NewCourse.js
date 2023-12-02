0e5cdc5d264e8015d51038ac8dd6e30698beb961
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-02 13:51:45 +0330

    feat: develop NewCourse component and add to main page

--- public/Components/NewCourse/NewCourse.js
+++ public/Components/NewCourse/NewCourse.js
@@ -5,10 +5,15 @@ template.innerHTML = `
 <div class="relative">
   <a id="NewCourseLink" class="w-full h-full block">
     <img id="NewCourseImg" alt="ghorbani-dev.ir" />
-    <span class="absolute right-2.5 top-2.5 flex-center w-12 h-6 bg-sabzlearnGreen text-white rounded-xl font-DanaBold text-sm">100%</span>
+    <span id="NewCourseFree" class="absolute right-2.5 top-2.5 hidden w-12 h-6 bg-sabzlearnGreen text-white rounded-xl font-DanaBold text-sm">100%</span>
     <div class="px-5 pt-3.5 pb-2.5">
-      <h4 id="NewCourseTitle" class="font-DanaBold h-12 line-clamp-2 text-zinc-700"></h4>
-      <p id="NewCourseSubtitle"></p>
+   
+    <a id="NewCourseTagOne" href="" class="inline-flex items-center justify-center text-xs text-sky-500 dark:text-yellow-400 bg-sky-500/10 dark:bg-yellow-400/10 py-1 px-1.5 mb-2.5 rounded">
+              </a>
+    <a id="NewCourseTagTwo" href="" class="inline-flex items-center justify-center text-xs text-sky-500 dark:text-yellow-400 bg-sky-500/10 dark:bg-yellow-400/10 py-1 px-1.5 mb-2.5 rounded">
+			           </a>
+      <h4 id="NewCourseTitle" class="font-DanaBold h-12 line-clamp-2 text-zinc-700 mb-3"></h4>
+      <p id="NewCourseSubtitle" class="font-Dana text-sm h-10 line-clamp-2 text-slate-500 dark:text-slate-400"></p>
       <div class="flex-between gap-2.5 flex-wrap text-slate-500 dark:text-slate-400 text-xs my-3">
           <div class="flex-center gap-1">
             <div class="flex-center gap-1">
@@ -32,7 +37,7 @@ template.innerHTML = `
           </div>
           <!-- line -->
           <p class="w-full h-px bg-gray-200 my-2"></p>
-          <div class="w-full flex-between">
+          <div class="w-full flex-between h-[46px]">
                 <div class="flex-center gap-1">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
@@ -41,7 +46,10 @@ template.innerHTML = `
                 </div>
                 <div class="flex flex-col items-center">
                  <span id="NewCoursePrice" class="line-through"></span>
-                 <span class="text-sabzlearnGreen font-DanaMd text-xl space-x-1.5">رایگان!</span>
+                 <div class="flex-center gap-1 ">
+                 <span id="NewCourseDiscountPrice" class="text-sabzlearnGreen font-DanaMd text-xl space-x-1.5"></span>
+                 <img id="TomanSvg" src="./assets/images/svg/toman.svg" alt="ghorbani-dev.ir" class="w-4 h-4"/>
+                 </div>
                 </div>
           </div>
       </div>
@@ -62,15 +70,22 @@ class NewCourse extends HTMLElement {
   connectedCallback() {
     this.shadowRoot.querySelector('#NewCourseLink').setAttribute( 'href' , this.getAttribute('NewCourseLink'))
     this.shadowRoot.querySelector('#NewCourseImg').setAttribute( 'src' , this.getAttribute('NewCourseImg'))
+    this.shadowRoot.querySelector('#NewCourseFree').classList.add(this.getAttribute('NewCourseFree'))
+    this.shadowRoot.querySelector('#NewCourseTagOne').innerHTML = this.getAttribute('NewCourseTagOne')
+    this.shadowRoot.querySelector('#NewCourseTagTwo').innerHTML = this.getAttribute('NewCourseTagTwo')
+    this.shadowRoot.querySelector('#NewCourseTagTwo').classList.add(this.getAttribute('NewCourseTagTwoClass'))
     this.shadowRoot.querySelector('#NewCourseTitle').innerHTML = this.getAttribute('NewCourseTitle')
+    this.shadowRoot.querySelector('#NewCourseSubtitle').innerHTML = this.getAttribute('NewCourseSubtitle')
     this.shadowRoot.querySelector('#NewCourseTeacher').innerHTML = this.getAttribute('NewCourseTeacher')
     this.shadowRoot.querySelector('#NewCourseTotalTime').innerHTML = this.getAttribute('NewCourseTotalTime')
     this.shadowRoot.querySelector('#NewCourseTotalStudents').innerHTML = this.getAttribute('NewCourseTotalStudents')
-this.shadowRoot.querySelector('#NewCoursePrice').innerHTML = this.getAttribute('NewCoursePrice')
+    this.shadowRoot.querySelector('#NewCoursePrice').innerHTML = this.getAttribute('NewCoursePrice')
+    this.shadowRoot.querySelector('#NewCourseDiscountPrice').innerHTML = this.getAttribute('NewCourseDiscountPrice')
+    this.shadowRoot.querySelector('#TomanSvg').classList.add(this.getAttribute('TomanSvg')) 
   }
 
   static observedAttributes() {
-    return ['NewCourseLink' ,'NewCourseImg' , 'NewCourseTitle' , 'NewCourseTeacher' , 'NewCourseTotalTime' , 'NewCourseTotalStudents' , 'NewCoursePrice']
+    return ['NewCourseLink' ,'NewCourseImg' , 'NewCourseFree', 'NewCourseTagOne' , 'NewCourseTagTwo' , 'NewCourseTagTwoClass ' , 'NewCourseTitle' , 'NewCourseSubtitle' , 'NewCourseTeacher' , 'NewCourseTotalTime' , 'NewCourseTotalStudents' , 'NewCoursePrice' , 'NewCourseDiscountPrice' , 'TomanSvg']
   }
 }
 
