efa84bbf2935425acc3076a8eec47582342edf53
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-03 19:04:41 +0330

    fix: bug in show more btn text in course page

--- public/assets/scripts/curse.js
+++ public/assets/scripts/curse.js
@@ -1,8 +1,17 @@
 const courseContent = document.querySelector('.CourseFullDescSection')
 const ShowFullDescBtn = document.querySelector('#ShowFullDescBtn')
 const CourseFullDescShadow = document.querySelector('.CourseFullDescShadow')
+const ShowMoreText = document.querySelector('#ShowMoreText')
+const ShowMoreSvg = document.querySelector('#ShowMoreSvg')
+
 
 ShowFullDescBtn.addEventListener('click' , () => {
     courseContent.classList.toggle('max-h-full')
     CourseFullDescShadow.classList.toggle('hidden')
+    ShowMoreSvg.classList.toggle('rotate-180')
+    if(ShowMoreSvg.classList.contains('rotate-180')){
+        ShowMoreText.innerHTML = "مشاهده کمتر"
+    }else{
+        ShowMoreText.innerHTML = "مشاهده بیشتر"
+    }
 })
