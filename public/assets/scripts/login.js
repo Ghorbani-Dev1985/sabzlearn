d30b978efa15c2247e642414242683f6c2d95311
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-05 16:20:05 +0330

    feat: develop toggle show and hide password input

---
+++ public/assets/scripts/login.js
@@ -0,0 +1,15 @@
+const $ = document;
+const LoginPassInput = $.querySelector('#LoginPassInput')
+const HiddenPassSvg = $.querySelector('#HiddenPassSvg')
+const ShowPassSvg = $.querySelector('#ShowPassSvg')
+
+HiddenPassSvg.addEventListener('click' ,() => {
+    LoginPassInput.setAttribute('type' , 'text')
+    HiddenPassSvg.classList.toggle('hidden')
+    ShowPassSvg.classList.toggle('hidden')
+})
+ShowPassSvg.addEventListener('click' , () => {
+    LoginPassInput.setAttribute('type' , 'password')
+    HiddenPassSvg.classList.toggle('hidden')
+    ShowPassSvg.classList.toggle('hidden')
+})
