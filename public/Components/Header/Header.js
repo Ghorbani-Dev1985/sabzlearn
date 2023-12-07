6702f6e944d1ff7d886e6acc9a563b2b67360896
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-07 15:56:40 +0330

    feat: develop overlay of profile section

--- public/Components/Header/Header.js
+++ public/Components/Header/Header.js
@@ -50,7 +50,7 @@ template.innerHTML = `
     <!-- main header -->
     <section class="bg-white">    
     <div class="container">
-        <div class="flex-between gap-4 py-6 relative z-50">
+        <div class="flex-between gap-4 py-6 relative ">
             <div class="flex-center gap-2">
                <!-- right side -->
                <img src="./assets/images/logo/Logo.png" alt="logo" />
@@ -180,7 +180,7 @@ template.innerHTML = `
     </section>
     <!-- mobile nav -->
 <!-- drawer component -->
-<div id="drawer-navigation" class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-all translate-x-full duration-500 shadow-6 bg-white w-64 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
+<div id="drawer-navigation" class="fixed top-0 right-0 h-screen p-4 overflow-y-auto transition-all translate-x-full duration-500 shadow-6 bg-white w-64 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
 <button id="CloseDrawer" type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -301,10 +301,9 @@ template.innerHTML = `
 </div>
 </div>
 </header>
+
 `;
-function MobileNavToggler() {
-  console.log("dsd");
-}
+
 class Header extends HTMLElement {
   constructor() {
     super();
@@ -324,7 +323,7 @@ class Header extends HTMLElement {
     const Blogs = this.shadowRoot.querySelector("#Blogs");
     const BlogsSubmenu = this.shadowRoot.querySelector("#BlogsSubmenu");
     const Python = this.shadowRoot.querySelector("#Python");
-    const PythonSubmenu = this.shadowRoot.querySelector("#PythonSubmenu");
+    const PythonSubmenu = this.shadowRoot.querySelector("#PythonSubmenu"); 
     const LoginRegisterBody =
       this.shadowRoot.querySelector("#LoginRegisterBody");
     const userProfileDropdown = this.shadowRoot.querySelector(
@@ -338,8 +337,9 @@ class Header extends HTMLElement {
       LoginRegisterBody.innerHTML = `<button id="ShowProfileDropDown" class="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 text-slate-500 dark:bg-gray-800 dark:text-gray-500">
        <img src="./assets/images/user.png" alt="Ghorbani-dev.ir" class="object-cover w-16 h-12 rounded-full inline-block" loading="lazy" />
       </button>  
+      <section id="Overlay" class="hidden fixed w-full h-full top-0 left-0 bg-black/40 z-40 md:backdrop-blur transition-all"></section>
       <!-- After Login  -->
-      <div class="hidden absolute left-0 top-full pt-4 z-10 transition-all show" id="UserProfileDropdown">
+      <div class="hidden absolute left-0 top-full pt-4 z-50  transition-all show" id="UserProfileDropdown">
          <div class="w-[278px] bg-white shadow-2xl dark:bg-gray-700 py-5 px-6 rounded-2xl border-b-4 border-sabzlearnGreen">
              <!-- User Info -->
              <div class="flex items-center border-b border-b-gray-200 dark:border-b-slate pb-5 mb-2">
@@ -361,15 +361,19 @@ class Header extends HTMLElement {
                                            </a>
                                        <a href="https://sabzlearn.ir/my-account/courses" class="flex items-center justify-between text-zinc-700 dark:text-white px-2.5 h-[46px] rounded-xl hover:bg-gray-100 dark:hover:bg-slate transition-colors">
                      <span class="flex items-center gap-x-3">
-                   
-                   
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+</svg>
+                
                          دوره های من                                </span>
                                            </a>
                                        <a href="https://sabzlearn.ir/my-account/tickets" class="flex items-center justify-between text-zinc-700 dark:text-white px-2.5 h-[46px] rounded-xl hover:bg-gray-100 dark:hover:bg-slate transition-colors">
                      <span class="flex items-center gap-x-3">
                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
-</svg>
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                   </svg>
+                   
+                   
 
                    
                          تیکت های پشتیبانی                                </span>
@@ -400,8 +404,14 @@ class Header extends HTMLElement {
       `;
       const ShowProfileDropDown = this.shadowRoot.querySelector('#ShowProfileDropDown')
       const UserProfileDropdown = this.shadowRoot.querySelector('#UserProfileDropdown')
+      const Overlay = this.shadowRoot.querySelector("#Overlay");
       ShowProfileDropDown.addEventListener('click' , ()=> {
         UserProfileDropdown.classList.toggle('hidden')
+        Overlay.classList.remove('hidden')
+      })
+      Overlay.addEventListener('click' , () => {
+        UserProfileDropdown.classList.add('hidden')
+        Overlay.classList.add('hidden')
       })
     })
     } else {
