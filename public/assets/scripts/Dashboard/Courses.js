ad8da23484188db546d6a750009348c3fb87b431
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-13 18:45:44 +0330

    feat: develop delete course in dashboard panel

--- public/assets/scripts/Dashboard/Courses.js
+++ public/assets/scripts/Dashboard/Courses.js
@@ -2,14 +2,17 @@ import {
   GetAllCourses,
   CreateNewCourse,
   PrepareCreateCourseFrom,
+  DeleteCourse
 } from "./Funcs/Courses.js";
+
+window.DeleteCourse = DeleteCourse;
 
 window.addEventListener("load", () => {
   const AddNewCourseBtn = document.querySelector("#AddNewCourseBtn");
   GetAllCourses();
   PrepareCreateCourseFrom();
   AddNewCourseBtn.addEventListener("click", (event) => {
-    event.preventDefault();
+   event.preventDefault();
     CreateNewCourse();
   });
 });
--- public/assets/scripts/Dashboard/Funcs/Courses.js
+++ public/assets/scripts/Dashboard/Funcs/Courses.js
@@ -59,7 +59,7 @@ const GetAllCourses = async () => {
         </div>
         </td>
         <td>
-        <div class="text-rose-500 cursor-pointer">
+        <div onClick="DeleteCourse('${course._id}')" class="text-rose-500 cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
       </svg>
@@ -129,32 +129,73 @@ const CreateNewCourse = async () => {
     },
     body: formData,
   });
-  if (res.ok) {
-    Swal.fire({
-      icon: "success",
-      title: "دوره جدید با موفقیت ثبت شد",
-      showCancelButton: false,
-      showConfirmButton: true,
-      timer: 10000,
-      confirmButtonColor: "#3085d6",
-      confirmButtonText: "مشاهده کلیه دوره ها",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        GetAllCourses();
-      }
-    });
-    // CourseNameInput.value = '';
-    // CoursePriceInput.value = '';
-    // CourseDescription.value = '';
-    // CourseShortNameInput.value = '';
-    // CourseSupportInput.value = '';
+   console.log(res);
+  
+  if (res.status === 201) {
+  Swal.fire({
+    icon: "success",
+    title: "دوره جدید با موفقیت ثبت شد",
+    showCancelButton: false,
+    showConfirmButton: true,
+    confirmButtonColor: "#3085d6",
+    confirmButtonText: "مشاهده کلیه دوره ها",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("ok");
+     // GetAllCourses();
+    }
+  });
   } else {
     ShowSwalAlert(
       "error",
       "خطایی در ثبت دوره جدید رخ داده است",
       "مشاهده کلیه دوره ها"
-    );
-  }
+      );
+    }
+    // CourseNameInput.value = '';
+    // CoursePriceInput.value = '';
+    // CourseDescription.value = '';
+    // CourseShortNameInput.value = '';
+    // CourseSupportInput.value = '';
 };
 
-export { GetAllCourses, CreateNewCourse, PrepareCreateCourseFrom };
+const DeleteCourse = async (courseID) => {
+ Swal.fire({
+      title: "آیا برای حذف دوره مطمعن هستید؟",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#f43f5e",
+      cancelButtonColor: "#3f3f46",
+      confirmButtonText: "حذف",
+      cancelButtonText: "انصراف"
+    }).then( async (result) => {
+      if (result.isConfirmed) {
+        const res = await fetch(`http://localhost:4000/v1/courses/${courseID}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${GetToken()}`,
+          },
+        });
+        if(res.ok){
+          GetAllCourses();
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "دوره مورد نظر با موفقیت حذف گردید",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }else{
+          Swal.fire({
+            position: "top-center",
+            icon: "error",
+            title: "خطایی در روند حذف دوره ایجاد گردید",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      }
+    });
+}
+
+export { GetAllCourses, CreateNewCourse, PrepareCreateCourseFrom ,DeleteCourse};
